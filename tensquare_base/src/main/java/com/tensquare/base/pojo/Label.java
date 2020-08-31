2285d0a567a6a078aa99422069ee26460751626b
mingli <mingli2015md@gmail.com>
2020-08-31 19:31:54 +0200

    Initial commit

---
+++ tensquare_base/src/main/java/com/tensquare/base/pojo/Label.java
@@ -0,0 +1,67 @@
+package com.tensquare.base.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_label")
+public class Label implements Serializable {
+
+    @Id
+    private String id;
+    private String labelname;
+    private String state;
+    private long count;
+    private long fans;
+    private String recommend;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLabelname() {
+        return labelname;
+    }
+
+    public void setLabelname(String labelname) {
+        this.labelname = labelname;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    public long getFans() {
+        return fans;
+    }
+
+    public void setFans(long fans) {
+        this.fans = fans;
+    }
+
+    public String getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(String recommend) {
+        this.recommend = recommend;
+    }
+}
